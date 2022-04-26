251486677fdb9d8e4d5c3cd268bd6aa7ad77ca55
Misawa Akihiko <misawa.akihiko@gmail.com>
2022-04-27 01:06:55 +0900

    プレイヤーの名前登録

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="home">
+    <div class="flex justify-center m-4">
+      <div class="w-1/5">
+        <label
+          class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+          for="inline-full-name"
+        >
+          名前：
+        </label>
+      </div>
+      <div class="w-3/5">
+        <input
+          type="text"
+          id="home--name-input"
+          v-model="name"
+          class="shadow appearance-none border border-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+        />
+      </div>
+      <div class="w-1/5">
+        <button
+          class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded"
+          @click="registerName"
+        >
+          登録
+        </button>
+      </div>
+    </div>
     <drawing-canvas
       name="aaa"
       @draw="sendDraw"
@@ -23,35 +49,36 @@ export default defineComponent({
   },
   setup() {
     const displayCanvas = ref();
-    const socket = io("http://localhost:3000");
+    const name = ref<string>("");
 
+    const socket = io("http://localhost:3000");
     socket.on("connect", () => {
       console.log(socket.id);
     });
-
     socket.on("draw line", (payload) => {
       console.log(payload);
       const { x, y } = payload;
       if (!displayCanvas.value) return;
       displayCanvas.value.draw(x, y);
     });
-
     socket.on("start draw", (payload) => {
       console.log(payload);
       const { x, y } = payload;
       if (!displayCanvas.value) return;
       displayCanvas.value.drawStart(x, y);
     });
-
     socket.on("end draw", () => {
       if (!displayCanvas.value) return;
       displayCanvas.value.drawEnd();
     });
-
     socket.on("delete all", () => {
       if (!displayCanvas.value) return;
       displayCanvas.value.deleteAll();
     });
+
+    const registerName = () => {
+      socket.emit("register name", name.value);
+    };
 
     const sendDraw = (payload: { x: number; y: number }): void => {
       socket.emit("draw line", payload);
@@ -73,7 +100,9 @@ export default defineComponent({
       socket.emit("delete all");
     };
     return {
+      name,
       displayCanvas,
+      registerName,
       sendDraw,
       sendStartDraw,
       sendEndDraw,
@@ -82,3 +111,10 @@ export default defineComponent({
   },
 });
 </script>
+<style scoped>
+.home--name-input {
+  border: 1px solid #333;
+  border-radius: 3px;
+  padding: 5px;
+}
+</style>
