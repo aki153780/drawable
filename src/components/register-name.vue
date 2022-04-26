ecaf177ac7e283fe4c32c79b60eb7544ce12b8de
Misawa Akihiko <misawa.akihiko@gmail.com>
2022-04-27 01:25:40 +0900

    名前登録部分をコンポーネント化

---
+++ src/components/register-name.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex justify-center m-4">
+    <div class="w-1/5">
+      <label
+        class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+        for="inline-full-name"
+      >
+        名前：
+      </label>
+    </div>
+    <div class="w-3/5">
+      <input
+        type="text"
+        id="home--name-input"
+        v-model="name"
+        class="shadow appearance-none border border-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+      />
+    </div>
+    <div class="w-1/5">
+      <button
+        class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded"
+        @click="registerName"
+      >
+        登録
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, SetupContext } from "vue";
+
+export default defineComponent({
+  setup(props, { emit }: SetupContext) {
+    const name = ref<string>("");
+
+    const registerName = () => {
+      emit("register-name", name.value);
+    };
+    return {
+      name,
+      registerName,
+    };
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,6 @@
 <template>
   <div class="home">
-    <div class="flex justify-center m-4">
-      <div class="w-1/5">
-        <label
-          class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-          for="inline-full-name"
-        >
-          名前：
-        </label>
-      </div>
-      <div class="w-3/5">
-        <input
-          type="text"
-          id="home--name-input"
-          v-model="name"
-          class="shadow appearance-none border border-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-        />
-      </div>
-      <div class="w-1/5">
-        <button
-          class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded"
-          @click="registerName"
-        >
-          登録
-        </button>
-      </div>
-    </div>
+    <register-name @registerName="registerName" />
     <drawing-canvas
       name="aaa"
       @draw="sendDraw"
@@ -39,17 +14,18 @@
 
 <script lang="ts">
 import { defineComponent, SetupContext, ref } from "vue";
-import DrawingCanvas from "@/components/drawing-canvas.vue"; // @ is an alias to /src
+import DrawingCanvas from "@/components/drawing-canvas.vue";
+import RegisterName from "@/components/register-name.vue";
 import { io } from "socket.io-client";
 
 export default defineComponent({
   name: "Home",
   components: {
+    RegisterName,
     DrawingCanvas,
   },
   setup() {
     const displayCanvas = ref();
-    const name = ref<string>("");
 
     const socket = io("http://localhost:3000");
     socket.on("connect", () => {
@@ -76,8 +52,8 @@ export default defineComponent({
       displayCanvas.value.deleteAll();
     });
 
-    const registerName = () => {
-      socket.emit("register name", name.value);
+    const registerName = (name: string) => {
+      socket.emit("register name", name);
     };
 
     const sendDraw = (payload: { x: number; y: number }): void => {
@@ -111,10 +87,3 @@ export default defineComponent({
   },
 });
 </script>
-<style scoped>
-.home--name-input {
-  border: 1px solid #333;
-  border-radius: 3px;
-  padding: 5px;
-}
-</style>
